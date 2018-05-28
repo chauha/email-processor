aa8053c2baa076e9e707d304dc6f5e9ef53799eb
ashish.chauhan <chauhan.ashish004@gmail.com>
2018-05-28 19:51:18 +0530

    Update email_process.sh

--- email_process.sh
+++ email_process.sh
@@ -8,7 +8,8 @@ dir=target
 ##jar file name
 jar_name=email-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-mvn clean compile assembly:single 
+#commenting / enable if you have mvn in your path.
+##mvn clean compile assembly:single 
 
 ## Launching application 
-java  -jar $dir/$jar_name
+java  -jar $jar_name
