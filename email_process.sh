7786f7a5ce3c39a0a7587ff03af1149a8c7906a8
Chauhan <Ashish.Chauhan2@emc.com>
2018-05-28 19:32:01 +0530

    change launcher script with new jar name

--- email_process.sh
+++ email_process.sh
@@ -6,9 +6,9 @@
 dir=target
 
 ##jar file name
-jar_name=email-0.0.1-SNAPSHOT.jar
+jar_name=email-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 
-mvn clean install 
+mvn clean compile assembly:single 
 
 ## Launching application 
-java  -cp "$LIB/*" -jar $dir/$jar_name
+java  -jar $dir/$jar_name
